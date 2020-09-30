33998ccb918dfcd8972ee3603ca628b6844a0e87
user86 <kshitij.kotasthane@gmail.com>
2020-09-30 21:09:45 +0530

    gmoudycfeg

--- init.py
+++ init.py
@@ -19,7 +19,7 @@ num_files = int(sys.argv[1])  # Number of garbage files
 garbage_len = int(sys.argv[2])  # Length of garbage content in each file
 
 def get_random_string(length):
-    """ Generate randdm string of fixed length """
+    """ Generate random string of fixed length """
     letters = string.ascii_lowercase
     res_str = ''.join(random.choice(letters) for i in range(length))
     return res_str
@@ -47,15 +47,25 @@ def del_src_dir():
         shutil.rmtree("./src")
         print("Garbage taken out")
 
-def branch_and_pr(no_pr=4):
+def branch_and_pr():
     """ Create a branch, make a commit and a PR"""
-    #br_name = get_random_str(10)
-    #os.system(f"git checkout -b ")
-    
+    try:
+        br_name = get_random_string(10)
+        os.system(f"git checkout -b {br_name}")
+        create_files(num_files, garbage_len)
+        os.system(f"git add .")
+        os.system(f"git commit -m {br_name}")
+        os.system(f"git push -u origin master")
+        os.system(f"gh pr create --base master --fill")
+        os.system(f"git checkout master")
+        print("It works !")
+    except Exception as e:
+        print(e)
 
 def main():
-    create_files(num_files, garbage_len)
-    del_src_dir()
+    for i in range(4):
+        branch_and_pr()
 
 if __name__ == "__main__":
     main()
+
